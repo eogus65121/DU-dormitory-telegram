bd2781193ca9db39f4945d50c6954ece249f2070
eogus65121 <eogus6512@gmail.com>
2021-06-19 05:21:13 +0900

    Update config.py
    biho_RC 페이지 권한문제 보류

--- DU/config.py
+++ DU/config.py
@@ -9,5 +9,6 @@ def get_resident():
 def get_resignation():
     return json.load(open("../data/resignation.json", "r", encoding="utf-8"))
 
+# biho_RC 페이지는 권한문제로 잠시 보류
 def get_biho_RC():
     return json.load(open("../data/biho_RC.json", "r", encoding="utf-8"))

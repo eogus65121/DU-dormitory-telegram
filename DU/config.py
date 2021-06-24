94ad4e0fad843c91a37a8f9809efb8a3d9dfe6ad
eogus65121 <eogus6512@gmail.com>
2021-06-24 16:35:50 +0900

    Update config.py
    get json 코드 추가

--- DU/config.py
+++ DU/config.py
@@ -18,3 +18,11 @@ def get_resignation():
 # biho_RC 페이지는 권한문제로 잠시 보류
 def get_biho_RC():
     return json.load(open("../data/biho_RC.json", "r", encoding="utf-8"))
+
+#공지 데이터 통합
+def get_dormitory():
+    return json.load(open("../data/dormitory.json", "r", encoding="utf-8"))
+
+# user data
+def get_user():
+    return json.load(open("../data/user.json", "r", encoding="utf-8"))

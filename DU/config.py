8b3a42a1f0725f1f5d42b02b6d863d6df8557254
eogus65121 <eogus6512@gmail.com>
2021-06-24 17:50:11 +0900

    Update config.py
    더미코드 삭제

--- DU/config.py
+++ DU/config.py
@@ -3,23 +3,11 @@ import json
 def get_config():
     return json.load(open("../data/config.json", "r", encoding="utf-8"))
 
-#일반공지
-def get_general():
-    return json.load(open("../data/general.json", "r", encoding="utf-8"))
-
-#자치회
-def get_resident():
-    return json.load(open("../data/resident.json", "r", encoding="utf-8"))
-
-#입퇴사
-def get_resignation():
-    return json.load(open("../data/resignation.json", "r", encoding="utf-8"))
-
 # biho_RC 페이지는 권한문제로 잠시 보류
 def get_biho_RC():
     return json.load(open("../data/biho_RC.json", "r", encoding="utf-8"))
 
-#공지 데이터 통합
+#공지 데이터 통합(일반공지, 자치회, 입퇴사)
 def get_dormitory():
     return json.load(open("../data/dormitory.json", "r", encoding="utf-8"))
 

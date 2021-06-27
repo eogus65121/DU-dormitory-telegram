ebd91ff3bc63bb7c5da6b4d5acc8e1488cb0457d
eogus65121 <eogus6512@gmail.com>
2021-06-27 17:58:47 +0900

    Update telegram_send.py
    text, url 전송 코드 추가

--- DU/telegram_send.py
+++ DU/telegram_send.py
@@ -2,7 +2,11 @@ import telegram
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def send_message(chat_id, config, content, url):
+def telegram_send_message_url(chat_id, config, content, url):
     bot = telegram.Bot(token=config['token'])
     reply_markup =[[InlineKeyboardButton(text="바로가기", url=url)]]
     bot.sendMessage(chat_id=chat_id, text=content, reply_markup=InlineKeyboardMarkup(reply_markup))
+
+def telegram_send_message(chat_id, config, text):
+    bot = telegram.Bot(token=config['token'])
+    bot.sendMessage(chat_id=chat_id, text = text)

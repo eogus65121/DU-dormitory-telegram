280877e43a15e5829f2e999c71881f050837d6e3
eogus65121 <eogus6512@gmail.com>
2021-06-29 01:06:22 +0900

    Update main.py
    관리자 권한 확인 server로 이동

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ CHOOSING, TYPING_FUNC = range(2)
 def start(update: Update, context: CallbackContext):
     reply_text = "반갑습니다. 대구대학교 기숙사 공지 알리미 입니다. \n '챗봇시작' 버튼 선택 혹은 '챗봇시작'을 입력해주세요"
     update.message.reply_text(reply_text, reply_markup=markup_start)
+    return ConversationHandler.END
 
 
 # 기존 chat_id가 데이터에 저장되어 있는지 확인
@@ -49,22 +50,9 @@ def check_user(chat_id, user_data):
 
     return flag
 
-# 관리자 기능 실행 시 권한 파악
-def check_admin(chat_id, user_data):
-    flag = False
-    for comp_user in user_data['admin_user']:
-        if chat_id not in comp_user.values():
-            continue
-        else:
-            flag = True
-            return flag
-
-    return flag
-
 # 공지 echo 메시지 전송
 def echo_notice(update:Update, context: CallbackContext) -> int:
     user_text = update.message.text
-    print(user_text)
     server_notice_echo(user_text)
     return ConversationHandler.END
 
@@ -85,10 +73,9 @@ def task(update:Update, context: CallbackContext) -> int:
     chat_id = update.message.chat_id
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text1)
     user_data = get_admin()
-    print("user checking admin...")
+    logging.info("user checking admin...")
 
     if check_admin(chat_id, user_data):
-        print("admin이 맞음 해당 기능 실행")
         update.message.reply_text("관리자 인증 성공", reply_markup=markup_func)
         return CHOOSING
     else:
@@ -108,15 +95,15 @@ def DU_main(update:Update, context: CallbackContext):
     chat_id = update.message.chat.id
 
     user_data = get_user()
-    print("user_data load success...")
+    logging.info("user_data load success...")
 
-    print("user checking start...")
+    logging.info("user checking start...")
     if check_user(chat_id, user_data):
         user_data['user'].append({"chat_id":chat_id})
         json.dump(user_data, open("../data/user.json", "w", encoding='utf-8'), ensure_ascii=False, indent=4)
-        print("user_data dump success...")
+        logging.info("user_data dump success...")
     else:
-        print("user exist")
+        logging.info("user exist...")
 
     reply_text = "자동 공지 서비스가 시작되었습니다. \n 게시판에 새로운 글이 업로드 시 알림이 전송됩니다." 
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
@@ -124,17 +111,13 @@ def DU_main(update:Update, context: CallbackContext):
     firstrun = True
 
     parser(firstrun)
+    return ConversationHandler.END
 
 
 def main():
     config = get_config()
     updater = Updater(token=config['token'])
     dispatcher = updater.dispatcher
-
-    #start_handler = CommandHandler('start', start)
-
-    #message_handler = MessageHandler(Filters.regex('^챗봇시작$'), DU_main)
-    #dispatcher.add_handler(start_handler)
 
     task_handler = ConversationHandler(
         entry_points=[CommandHandler('task', task)],

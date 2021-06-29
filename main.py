c0efede739762740298c55530ca2049f0a3bb884
eogus65121 <eogus6512@gmail.com>
2021-06-29 20:35:03 +0900

    Update main.py
    코드 수정, 더미코드 제거

--- main.py
+++ main.py
@@ -25,21 +25,23 @@ from telegram.ext import (
 
 reply_start = [['챗봇시작']]
 task_func_noti = [['전체공지']]
-task_func_add_adm = [['관리자 추가']]
-task_func_end = [['Done']]
+task_func_adm_manage = [['관리자 관리']]
+task_func_end = [['종료']]
 reply_func = [['기능선택']]
-reply_YS = [['Yes'], ['No']]
+reply_add = [['추가']]
+reply_delete = [['삭제']]
 
 
 markup_start = ReplyKeyboardMarkup(reply_start, one_time_keyboard=True)
 markup_func_noti = ReplyKeyboardMarkup(task_func_noti, one_time_keyboard=True)
-markup_func_add_adm = ReplyKeyboardMarkup(task_func_add_adm, one_time_keyboard=True)
+markup_func_adm_manage = ReplyKeyboardMarkup(task_func_adm_manage, one_time_keyboard=True)
 markup_func_end = ReplyKeyboardMarkup(task_func_end, one_time_keyboard=True)
 markup_func = ReplyKeyboardMarkup(reply_func, one_time_keyboard=True)
-markup_YS = ReplyKeyboardMarkup(reply_YS, one_time_keyboard=True)
+markup_add = ReplyKeyboardMarkup(reply_add, one_time_keyboard=True)
+markup_delete = ReplyKeyboardMarkup(reply_delete, one_time_keyboard=True)
 
 #CHOOSING, NOTI_FUNC, ADD_ADMIN = range(3)
-CHOOSING, ACTION, NOTI_FUNC, ADD_ADM, FUN_END = range(5)
+CHOOSING, ACTION, NOTI_FUNC, ADM_MANAGE, FUN_END, ADD, DELETE = range(7)
 
 # telegram bot 첫 시작 /start command handler
 def start(update: Update, context: CallbackContext):
@@ -73,15 +75,23 @@ def notice_task(update:Update, context: CallbackContext) -> int:
     reply_text = "전체공지 기능입니다. 내용 입력 시 모든 사용자에게 전송됩니다."
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
 
-def add_admin_func(update:Update, context: CallbackContext) -> int:
-    reply_text = "관리자 계정 추가하기 기능입니다. id를 입력시 관리자로 등록됩니다."
-    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
-    return ConversationHandler.END
+def adm_manage_func(update:Update, context: CallbackContext) -> int:
+    Keyboard = [
+        [
+        InlineKeyboardButton("추가", callback_data='ADD'),
+        InlineKeyboardButton("제거", callback_data='DELETE'),
+        ],
+        [InlineKeyboardButton("종료", callback_data='FUN_END')]
+    ]
+    reply_markup = InlineKeyboardMarkup(Keyboard)
+    update.message.reply_text("기능을 선택해주세요", reply_markup=reply_markup)
+    return ACTION
+    
 
-# def add_id_check(update:Update, context: CallbackContext) -> int:
-#     user_id = update.message.text
-#     reply_text = "추가를 워하는 id가 " + str(user_id) + "맞습니까? \n YES = 추가하기 // NO = 처음으로(/task 명령부터 재실행)"
-#     update.message.reply_text(reply_text, reply_markup=markup_YS)
+def add_id_check(update:Update, context: CallbackContext) -> int:
+    user_id = update.message.text
+    reply_text = "추가를 원하는 id가 " + str(user_id) + "맞습니까? \n YES = 추가하기 // NO = 처음으로(/task 명령부터 재실행)"
+    update.message.reply_text(reply_text, reply_markup=markup_YS)
 
 
 # def add_admin(update:Update, context:CallbackContext, data) -> int:
@@ -115,21 +125,29 @@ def action(update:Update, context:CallbackContext):
         reply_text = "하단 전체공지 버튼을 눌러주세요"
         context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_func_noti)
         return NOTI_FUNC
-    elif query.data == "ADD_ADM":
-        reply_text = "하단 관리자 추가 버튼을 눌러주세요"
-        context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_func_add_adm)
-        return ADD_ADM
+    elif query.data == "ADM_MANAGE":
+        reply_text = "하단 관리자 관리 버튼을 눌러주세요"
+        context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_func_adm_manage)
+        return ADM_MANAGE
     elif query.data == "FUN_END":
         reply_text = "하단 종료 버튼을 눌러주세요"
         context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_func_end)
         return FUN_END
+    elif query.data == "ADD":
+        reply_text = "하단 추가 버튼을 눌러주세요"
+        context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_add)
+        return ADD
+    elif query.data == "DELETE":
+        reply_text = "하단 삭제 버튼을 눌러주세요"
+        context.bot.send_message(chat_id=chat_id, text =reply_text, reply_markup=markup_delete)
+        return DELETE
 
-
+# 관리자 기능 선택
 def func_choice(update:Update, context:CallbackContext) ->None :
     Keyboard = [
         [
         InlineKeyboardButton("전체공지", callback_data='NOTI_FUNC'),
-        InlineKeyboardButton("관리자추가", callback_data='ADD_ADM'),
+        InlineKeyboardButton("관리자 관리", callback_data='ADM_MANAGE'),
         ],
         [InlineKeyboardButton("종료", callback_data='FUN_END')]
     ]
@@ -137,13 +155,14 @@ def func_choice(update:Update, context:CallbackContext) ->None :
     update.message.reply_text("기능을 선택해주세요", reply_markup=reply_markup)
     return ACTION
 
+
 # conversation handler 종료
 def done(update: Update, context: CallbackContext) -> int:
     reply_text = "관리자 기능이 종료되었습니다."
     context.bot.send_message(chat_id=update.effective_chat.id, text = reply_text)
     return ConversationHandler.END
 
-
+# 개인 id 확인
 def id_check(update: Update, context: CallbackContext) -> int:
     my_chat_id = update.message.chat.id
     reply_text = "나의 chat_id = " + str(my_chat_id)
@@ -191,14 +210,20 @@ def main():
                 MessageHandler(Filters.regex('^전체공지$'), notice_task),
                 MessageHandler(Filters.text & ~Filters.command, echo_notice)
             ],
-            ADD_ADM:[
-                MessageHandler(Filters.regex('^관리자 추가$'), add_admin_func)
+            ADM_MANAGE:[
+                MessageHandler(Filters.regex('^관리자 관리$'), adm_manage_func)
             ],
             FUN_END:[
                 MessageHandler(Filters.regex('^종료$'), done)
+            ],
+            ADD:[
+                MessageHandler(Filters.regex('^전체공지$'), notice_task)
+            ],
+            DELETE:[
+
             ]
         },
-        fallbacks=[MessageHandler(Filters.regex('^done$'), done)]
+        fallbacks=[MessageHandler(Filters.regex('^종료$'), done)]
     )
 
     dispatcher.add_handler(CommandHandler('start', start))
